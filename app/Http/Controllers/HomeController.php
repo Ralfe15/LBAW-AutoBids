5f36afe64b4a638c7c7cc629347d98b92fa71d0a
Luís Pedro Morais <up200800621@edu.fe.up.pt>
2022-11-22 01:44:16 +0000

    fixed home showing innactive auctions

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,10 +11,10 @@ class HomeController extends Controller
 {
 
     public function show() {
-        $auctions_latest =  Auction::orderBy('start_date', 'desc')->paginate(5);
-        $auctions_finishing =  Auction::orderBy('end_date', 'desc')->paginate(5);
-        $auctions_popular =  Auction::orderBy('number_bids', 'desc')->paginate(5);
-        
+        $auctions_latest =  Auction::where('active', true)->orderBy('start_date', 'desc')->paginate(5);
+        $auctions_finishing =  Auction::where('active', true)->orderBy('end_date', 'desc')->paginate(5);
+        $auctions_popular =  Auction::where('active', true)->orderBy('number_bids', 'desc')->paginate(5);
+
         return view("pages.default", ['auctions_latest' => $auctions_latest, 'auctions_finishing' => $auctions_finishing, 'auctions_popular' => $auctions_popular]);
     }
 }
