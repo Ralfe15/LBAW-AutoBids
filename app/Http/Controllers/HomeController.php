0ab7cced7e739d16ef77d91655eb247fd23bbc6e
Luís Pedro Morais <up200800621@edu.fe.up.pt>
2022-11-23 22:32:24 +0000

    simple search done

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -14,7 +14,6 @@ class HomeController extends Controller
         $auctions_latest =  Auction::where('active', true)->orderBy('start_date', 'desc')->paginate(5);
         $auctions_finishing =  Auction::where('active', true)->orderBy('end_date', 'desc')->paginate(5);
         $auctions_popular =  Auction::where('active', true)->orderBy('number_bids', 'desc')->paginate(5);
-
         return view("pages.default", ['auctions_latest' => $auctions_latest, 'auctions_finishing' => $auctions_finishing, 'auctions_popular' => $auctions_popular]);
     }
 }
