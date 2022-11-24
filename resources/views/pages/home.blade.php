8d44318f446c929c9ab50583c51e052d6acaf8a4
Luís Pedro Morais <up200800621@edu.fe.up.pt>
2022-11-24 00:41:09 +0000

    style changes

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -27,12 +27,5 @@
         <span>Price</span>
     </label>
 </div>
-@if(Auth::check())
-{{--    TODO: Handle these endpoints--}}
-<div class="button-wrapper">
-    <a class="central-button" href='{{route('my_auctions')}}'>Your owned auctions</a>
-    <a class="central-button" href=''>Your favorite auctions</a>
-    <a class="central-button" href='{{route('auctions')}}'>View active auctions</a>
-</div>
-@endif
+
 </body>
