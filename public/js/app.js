9563908693572a0a917366df7c1f851451686a58
Rafael Schmidt <ralfe.schmidt@gmail.com>
2022-12-19 17:39:18 +0000

    fix multiple auctions in one page fav

--- public/js/app.js
+++ public/js/app.js
@@ -15,12 +15,15 @@ function toggleFavorite(id, isfav) {
         }).then(response => response.json()
         ).then((response) => {
             if (response.success) {
-                const button = document.querySelector("#toggle" + id)
-                const icon = document.querySelector("#heart-icon" + id)
-                icon.className = "fa fa-heart"
-                button.textContent = "Remove from favorites: "
-                button.append(icon)
-                button.setAttribute("onclick", "toggleFavorite(" + id + ", 'true')")
+                const button = document.querySelectorAll("#toggle" + id)
+                const icon = document.querySelectorAll("#heart-icon" + id)
+                for (var i = 0; i < button.length; i++) {
+                    icon[i].className = "fa fa-heart"
+                    button[i].textContent = "Remove from favorites: "
+                    button[i].append(icon[i])
+                    button[i].setAttribute("onclick", "toggleFavorite(" + id + ", 'true')")
+                }
+
             } else {
                 throw new Error(response.message);
             }
@@ -37,12 +40,14 @@ function toggleFavorite(id, isfav) {
         }).then(response => response.json()
         ).then((response) => {
             if (response.success) {
-                const button = document.querySelector("#toggle" + id)
-                const icon = document.querySelector("#heart-icon" + id)
-                icon.className = "fa fa-heart-o"
-                button.textContent = "Add to favorites: "
-                button.append(icon)
-                button.setAttribute("onclick", "toggleFavorite(" + id + ", 'false')")
+                const button = document.querySelectorAll("#toggle" + id)
+                const icon = document.querySelectorAll("#heart-icon" + id)
+                for (var i = 0; i < button.length; i++) {
+                    icon[i].className = "fa fa-heart-o"
+                    button[i].textContent = "Add to favorites: "
+                    button[i].append(icon[i])
+                    button[i].setAttribute("onclick", "toggleFavorite(" + id + ", 'false')")
+                }
             } else {
                 throw new Error(response.message);
             }
